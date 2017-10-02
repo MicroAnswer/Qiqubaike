f670b1b9d4fd11afdec3c586cc7e4a1243b2323b
MicroAnswer <microanswer@outlook.com>
2017-10-02 13:03:48 +0800

    初始提交

---
+++ app/src/main/java/com/microanswer/qiqubaike/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.microanswer.qiqubaike.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.microanswer.qiqubaike.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/microanswer/qiqubaike/api/QiquApi.java
@@ -0,0 +1,17 @@
+package com.microanswer.qiqubaike.api;
+
+/**
+ * 崎岖百科API接口地址
+ * Created by Micro on 2017-10-2.
+ */
+
+public class QiquApi {
+
+    private static String HOST = "https://qiqu-api.uc.cn/";
+
+    /**
+     * 精选界面的banner内容接口
+     */
+    public static String JingXuan = HOST + "api/banner/jingxuan";
+
+}
